e3f72c9be8a41e2785ac7e8c2441e625e794afd7
kasader <casada980@gmail.com>
2026-08-21 11:59:08 +0900

    Use hugo Zen theme; adopt nix flake

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Hugo (Go) development environment.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.hugo
+            # Hugo Modules are Go modules; `hugo mod` shells out to both.
+            pkgs.go
+            pkgs.git
+            # Hugo dropped libsass, so extended-only SCSS themes need the
+            # dart-sass transpiler on PATH.
+            pkgs.dart-sass
+            pkgs.gopls
+            pkgs.golangci-lint
+          ];
+
+          shellHook = ''
+            echo "hugo dev shell ready"
+          '';
+        };
+      }
+    );
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kasader/kasader.github.io
+
+go 1.26.5
+
+require github.com/frjo/hugo-theme-zen/v7 v7.0.1 // indirect
